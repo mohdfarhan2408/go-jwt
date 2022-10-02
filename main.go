da0e840ae61381d68ed90f59427665769f1a66fe
farhan <farhanabdulrahman98@gmail.com>
2022-10-02 16:19:24 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go-jwt/controllers"
+	"go-jwt/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+
+	r := gin.Default()
+	r.POST("/signup", controllers.Signup)
+
+	r.Run()
+}
